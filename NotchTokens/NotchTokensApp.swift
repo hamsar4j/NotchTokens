ce4e236e2f72d91e68a5592f47d61cbba0dfea0e
hamsar4j <hamsarajs8@gmail.com>
2026-05-18 16:55:29 +0800

    Initial Commit

---
+++ NotchTokens/NotchTokensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchTokensApp.swift
+//  NotchTokens
+//
+//  Created by Hamsaraj S on 18/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchTokensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
